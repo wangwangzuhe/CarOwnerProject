cc50f28644b982c9eaa2025e4a562fa26f1215bb
qiaogangfeng <qiaogangfeng@cydeng.com>
2017-11-30 23:24:45 +0800

    订单完成判定是否跳转至评论页

--- pages/cancel/cancel.js
+++ pages/cancel/cancel.js
@@ -5,9 +5,41 @@ var wbs = require('../../utils/wbs.js');
 var session = require("../../utils/session");
 
 app.vaildPage({
+    data: {
+        showtxt:"您的订单已完成！"
+    },
+    onLoad(options) {
+        if(options.t==0){
+            this.setData({
+                showtxt:"您的订单已取消！"
+            });
+        }else{
+            this.checkowner();
+        }
+    },
+    checkowner(){
+        httpreq.request({
+            url: wbs.owner,
+            data: {
+                openId: wx.getStorageSync('openId')
+            }
+
+        },function(res){
+            var res = res.data;
+            
+            if (res.code == "finishNoComment") {
+               
+                wx.redirectTo({
+                    url: '../comment/comment'
+                });
+                
+            }
+        });
+    },
     tapgo:function(){
         wx.redirectTo({
             url: '../record/record'
         });
-    }
+    },
+    
 });
